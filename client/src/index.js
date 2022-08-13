a211fc86e3b99236aeb8231f81f346b356ef1173
Tanvir Hossain Sadi <htanvir.sadi@gmail.com>
2022-08-13 20:21:44 +0600

    updated url

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client
 
 
 const client = new ApolloClient({
-  uri: '/graphql',
+  uri: 'https://gql-quote.herokuapp.com/graphql',
   cache: new InMemoryCache(),
   headers:{
     authorization: localStorage.getItem('accessToken')
